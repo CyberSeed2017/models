72b6d5a03bf627dacc0c14667ac66d24cdba480c
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-10-09 19:00:10 -0400

    incremental prog

--- playtime.py
+++ playtime.py
@@ -9,8 +9,8 @@ from django.contrib.auth.models import User
 #System Administrator Role
 System_Administrator(date, uname):
     # requests
-    Date =
-    username =
+    Date = date
+    username = uname
     def __str__(self):
         return str(self.username)
 
@@ -18,57 +18,57 @@ System_Administrator(date, uname):
 #Doctor Role
 class Doctor(models.Model):
     # requests
-    Practice_Name =
-    Practice_Address =
-    Recovery_Phrase =
-    #Username =
-    username =
+    Practice_Name = practice_name
+    Practice_Address = address p_addr
+    Recovery_Phrase = r_phase
+    #username = uname
+    username = uname
     #created_by =
-    created_at =
+    created_at = create_date
     def __str__(self):
         return self.Practice_Name
 
 #Nurse Role
 class Nurse(models.Model):
     # requests
-    Practice_Name =
-    Practice_Address =
-    Associated_Doctors =
-    username =
-    created_at =
+    Practice_Name = practice_name
+    Practice_Address = address p_addr
+    Associated_Doctors = doc_colleagues
+    username = uname
+    created_at = create_date
     def __str__(self):
         return self.Practice_Name
 
 #Medical Administrator Role
 class Medical_Administrator(models.Model):
     # requests
-    Practice_Name =
-    Practice_Address =
-    Associated_Doctors =
-    Associated_Nurses =
-    username =
-    created_at =
+    Practice_Name = practice_name
+    Practice_Address = address p_addr
+    Associated_Doctors = doc_colleagues
+    Associated_Doctors = nurse_colleagues
+    username = uname
+    created_at = create_date
     def __str__(self):
         return self.Practice_Name
 
 #Insurance Administrator Role
 class Insurance_Administrator(models.Model):
     # requests
-    Company_Name =
-    Company_Address =
-    username =
-    created_at =
+    Company_Name = ins_company
+    Company_Address = address ins_address
+    username = uname
+    created_at = create_date
     def __str__(self):
         return self.Company_Name
 
 #Patient Role
 class Patient(models.Model):
     # requests
-    SSN =
-    Address =
-    DOB =
-    username =
-    created_at =
+    SSN = ssn
+    Address = address
+    DOB = dob
+    username = uname
+    created_at = create_date
     def __str__(self):
         return self.SSN
 
@@ -79,46 +79,46 @@ class Record(models.Model):
     lst =
     options =
     Record_Type =
-    Record_Date =
+    Record_Date = date
     Owner =
     Patient =
     Edit_Permissions =
     View_Permissions =
-    created_at =
+    created_at = create_date
     def __str__(self):
         return str(self.Record_ID)
 
 #Doctor Exam Record
 class Doctor_Exam_Record(models.Model):
     # requests
-    Date =
+    Date = date
     Doctor =
     Notes =
     Record =
-    created_at =
+    created_at = create_date
     def __str__(self):
         return str(self.id)
 
 #Diagnosis Record
 class Diagnosis_Record(models.Model):
     # requests
-    Date =
+    Date = date
     Doctor =
     Diagnosis =
     Record =
-    created_at =
+    created_at = create_date
     def __str__(self):
         return str(self.id)
 
 #Test Results Record
 class Test_Results_Record(models.Model):
     # requests
-    Date =
+    Date = date
     Doctor =
     Lab =
     Notes =
     Record =
-    created_at =
+    created_at = create_date
     def __str__(self):
         return str(self.id)
 
@@ -126,13 +126,13 @@ class Test_Results_Record(models.Model):
 #Insurance Claim Record
 class Insurance_Claim_Record(models.Model):
     # requests
-    Date =
+    Date = date
     Medical_Administrator =
     Amount =
     Status_Options =
     Status =
     Record =
-    created_at =
+    created_at = create_date
     def __str__(self):
         return str(self.id)
 
@@ -143,7 +143,7 @@ class Patient_Doctor_Correspondence_Record(models.Model):
     Doctor =
     Notes =
     Record =
-    created_at =
+    created_at = create_date
     def __str__(self):
         return str(self.id)
 
@@ -153,17 +153,16 @@ class Raw_Record(models.Model):
     Description =
     File =
     Record =
-    created_at =
+    created_at = create_date
     def __str__(self):
         return str(self.id)
 
 #Note
 class Note(models.Model):
     # requests
-    Date =
+    Date = date
     Text =
     Patient_Doctor_Correspondence =
-    created_at =
+    created_at = create_date
     def __str__(self):
         return str(self.id)
-
